21366ba6444cdf9aa15745db92232108e65915b7
linhdoan9513 <78831085+linhdoan9513@users.noreply.github.com>
2021-08-30 16:26:25 -0400

    "updated day 15"

--- day7-chess-game/src/BlackSquare.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Square from "./Square";
-
-const BlackSquare = (props) => {
-  return <Square color={"#3c8239"} position={props.position} />;
-};
-
-export default BlackSquare;
--- day7-chess-game/src/WhiteSquare.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Square from "./Square";
-
-const WhiteSquare = (props) => {
-  return <Square color={"#9fb39f"} position={props.position} />;
-};
-
-export default WhiteSquare;
